37c6731f93712b7fd38f26b8ae90b60086c4980b
stoically <stoically@protonmail.com>
2020-04-28 13:24:56 +0200

    refactor: cleanup

--- native/src/indexer.rs
+++ native/src/indexer.rs
@@ -1,15 +1,149 @@
 use anyhow::{bail, Result};
+use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
-use seshat::{Config, Connection, Database, Event, EventType, Language, LoadConfig, Profile};
-use std::path::PathBuf;
-
-mod message;
+use seshat::{
+    Config, Connection, CrawlerCheckpoint, Database, EventType, Language, LoadConfig, Profile,
+    SearchConfig,
+};
+use std::{collections::HashMap, path::PathBuf};
 
 use crate::Radical;
-use message::{
-    AddEventToIndex, AddHistoricEvents, DeleteEvent, FileEvent, InitEventIndex, Message,
-    SearchEventIndex, SearchResult, SearchResultContext, SearchResults,
-};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase", tag = "method")]
+pub enum Message {
+    InitEventIndex(InitEventIndex),
+    LoadCheckpoints,
+    IsEventIndexEmpty,
+    CommitLiveEvents,
+    AddEventToIndex { content: AddEventToIndex },
+    AddCrawlerCheckpoint { content: AddHistoricEvents },
+    AddHistoricEvents { content: AddHistoricEvents },
+    RemoveCrawlerCheckpoint { content: AddHistoricEvents },
+    SearchEventIndex { content: SearchEventIndex },
+    LoadFileEvents { content: LoadConfig },
+    DeleteEvent { content: DeleteEvent },
+    GetStats,
+    CloseEventIndex,
+    DeleteEventIndex,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InitEventIndex {
+    pub passphrase: Option<String>,
+    pub language: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AddEventToIndex {
+    pub ev: Value,
+    pub profile: Profile,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AddHistoricEvents {
+    pub checkpoint: Option<CrawlerCheckpoint>,
+    pub old_checkpoint: Option<CrawlerCheckpoint>,
+    pub events: Option<Vec<Events>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SearchEventIndex {
+    pub term: String,
+    pub config: SearchConfig,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DeleteEvent {
+    pub event_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SearchResults {
+    pub count: usize,
+    pub results: Vec<SearchResult>,
+    pub highlights: Vec<SearchHighlight>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SearchResult {
+    pub rank: f32,
+    pub result: Value,
+    pub context: SearchResultContext,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SearchResultContext {
+    pub events_before: Vec<Value>,
+    pub events_after: Vec<Value>,
+    pub profile_info: HashMap<String, Profile>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SearchHighlight {}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FileEvent {
+    pub event: Value,
+    pub profile: Profile,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Events {
+    pub event: Value,
+    pub profile: Profile,
+}
+
+// remove duplication once specialization lands
+// https://github.com/rust-lang/rust/issues/31844
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum Event {
+    #[serde(rename = "m.room.message")]
+    Message(EventMessage),
+    #[serde(rename = "m.room.name")]
+    Name(EventName),
+    #[serde(rename = "m.room.topic")]
+    Topic(EventTopic),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EventMessage {
+    pub event_id: String,
+    pub sender: String,
+    #[serde(rename = "origin_server_ts")]
+    pub server_ts: i64,
+    pub room_id: String,
+    pub content: EventMessageContent,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EventMessageContent {
+    pub body: String,
+    pub msgtype: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EventName {
+    pub event_id: String,
+    pub sender: String,
+    #[serde(rename = "origin_server_ts")]
+    pub server_ts: i64,
+    pub room_id: String,
+    pub name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EventTopic {
+    pub event_id: String,
+    pub sender: String,
+    #[serde(rename = "origin_server_ts")]
+    pub server_ts: i64,
+    pub room_id: String,
+    pub topic: String,
+}
 
 pub(crate) fn handle_message(radical: &mut Radical, message_in: Value) -> Result<Value> {
     let event_store = match message_in.get("eventStore") {
@@ -120,7 +254,7 @@ impl Indexer {
     }
 
     fn add_history_events(&mut self, message: AddHistoricEvents) -> Result<Value> {
-        let mut events: Vec<(Event, Profile)> = Vec::new();
+        let mut events: Vec<(seshat::Event, Profile)> = Vec::new();
         if let Some(events_in) = message.events {
             for event_in in events_in {
                 let profile = event_in.profile;
@@ -213,9 +347,9 @@ fn config(message: InitEventIndex) -> Config {
 
 fn convert_event(event: Value) -> Result<seshat::Event> {
     let source = serde_json::to_string(&event)?;
-    let event: message::Event = serde_json::from_value(event)?;
+    let event: Event = serde_json::from_value(event)?;
     let res = match event {
-        message::Event::Message(ev) => Event {
+        Event::Message(ev) => seshat::Event {
             event_type: EventType::Message,
             content_value: ev.content.body,
             msgtype: ev.content.msgtype,
@@ -225,7 +359,7 @@ fn convert_event(event: Value) -> Result<seshat::Event> {
             room_id: ev.room_id,
             source,
         },
-        message::Event::Name(ev) => Event {
+        Event::Name(ev) => seshat::Event {
             event_type: EventType::Name,
             content_value: ev.name,
             msgtype: None,
@@ -235,7 +369,7 @@ fn convert_event(event: Value) -> Result<seshat::Event> {
             room_id: ev.room_id,
             source,
         },
-        message::Event::Topic(ev) => Event {
+        Event::Topic(ev) => seshat::Event {
             event_type: EventType::Message,
             content_value: ev.topic,
             msgtype: None,
@@ -368,7 +502,7 @@ mod tests {
         let mut indexer = indexer(tmpdir.path());
 
         let profile = Profile::new("Alice", "");
-        let payload: message::AddEventToIndex = serde_json::from_value(json!({
+        let payload: AddEventToIndex = serde_json::from_value(json!({
             "ev": event_room_message_text(),
             "profile": profile
         }))
