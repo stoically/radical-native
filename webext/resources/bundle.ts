50ddd70fd0d9bf44e0a5180eb047f597e3fccaa4
stoically <stoically@protonmail.com>
2020-05-31 23:27:12 +0200

    fix: linter

--- webext/resources/bundle.ts
+++ webext/resources/bundle.ts
@@ -5,7 +5,7 @@
 let rpcId = 0;
 const rpcPromises: Map<number, any> = new Map();
 
-function handleMessage(message: any) {
+function rpcHandleMessage(message: any) {
   switch (message.method) {
     case "rpc":
       const rpcPromise = rpcPromises.get(message.rpcId);
@@ -22,7 +22,7 @@ function handleMessage(message: any) {
   }
 }
 
-function postMessage(type: string, message: any): Promise<any> {
+function rpcPostMessage(type: string, message: any): Promise<any> {
   return new Promise((resolve, reject) => {
     rpcId++;
     const rpcMessage = {
@@ -42,40 +42,40 @@ function postMessage(type: string, message: any): Promise<any> {
 
 class SeshatIndexManager {
   async supportsEventIndexing() {
-    return postMessage("seshat", { method: "supportsEventIndexing" });
+    return rpcPostMessage("seshat", { method: "supportsEventIndexing" });
   }
 
   async initEventIndex() {
-    return postMessage("seshat", { method: "initEventIndex" });
+    return rpcPostMessage("seshat", { method: "initEventIndex" });
   }
 
   async addEventToIndex(ev: any, profile: any) {
-    return postMessage("seshat", {
+    return rpcPostMessage("seshat", {
       method: "addEventToIndex",
       content: { ev, profile },
     });
   }
 
   async isEventIndexEmpty() {
-    return postMessage("seshat", { method: "isEventIndexEmpty" });
+    return rpcPostMessage("seshat", { method: "isEventIndexEmpty" });
   }
 
   async commitLiveEvents() {
-    return postMessage("seshat", { method: "commitLiveEvents" });
+    return rpcPostMessage("seshat", { method: "commitLiveEvents" });
   }
 
   async searchEventIndex(config: any) {
     const term = config.search_term;
     delete config.search_term;
 
-    return postMessage("seshat", {
+    return rpcPostMessage("seshat", {
       method: "searchEventIndex",
       content: { term, config },
     });
   }
 
   async addHistoricEvents(events: any, checkpoint: any, oldCheckpoint: any) {
-    return postMessage("seshat", {
+    return rpcPostMessage("seshat", {
       method: "addHistoricEvents",
       content: {
         events,
@@ -86,44 +86,44 @@ class SeshatIndexManager {
   }
 
   async addCrawlerCheckpoint(checkpoint: any) {
-    return postMessage("seshat", {
+    return rpcPostMessage("seshat", {
       method: "addCrawlerCheckpoint",
       content: { checkpoint },
     });
   }
 
   async removeCrawlerCheckpoint(oldCheckpoint: any) {
-    return postMessage("seshat", {
+    return rpcPostMessage("seshat", {
       method: "removeCrawlerCheckpoint",
       content: { oldCheckpoint },
     });
   }
 
   async loadFileEvents(args: any) {
-    return postMessage("seshat", {
+    return rpcPostMessage("seshat", {
       method: "loadFileEvents",
       content: { ...args },
     });
   }
 
   async loadCheckpoints() {
-    return postMessage("seshat", { method: "loadCheckpoints" });
+    return rpcPostMessage("seshat", { method: "loadCheckpoints" });
   }
 
   async closeEventIndex() {
-    return postMessage("seshat", { method: "closeEventIndex" });
+    return rpcPostMessage("seshat", { method: "closeEventIndex" });
   }
 
   async getStats() {
-    return postMessage("seshat", { method: "getStats" });
+    return rpcPostMessage("seshat", { method: "getStats" });
   }
 
   async deleteEventIndex() {
-    return postMessage("seshat", { method: "deleteEventIndex" });
+    return rpcPostMessage("seshat", { method: "deleteEventIndex" });
   }
 
   async deleteEvent(eventId: any) {
-    return postMessage("seshat", {
+    return rpcPostMessage("seshat", {
       method: "deleteEvent",
       content: { eventId },
     });
@@ -142,6 +142,39 @@ class PlatformPeg {
   set(plaf: any) {
     this.platform = plaf;
     this.platform.getEventIndexingManager = () => indexManager;
+
+    this.platform.getPickleKey = async (
+      userId: string,
+      deviceId: string
+    ): Promise<string | null> => {
+      console.log("getPickleKey", userId, deviceId);
+      return rpcPostMessage("keytar", {
+        method: "getPickleKey",
+        content: { userId, deviceId },
+      });
+    };
+
+    this.platform.createPickleKey = async (
+      userId: string,
+      deviceId: string
+    ): Promise<string | null> => {
+      console.log("createPickleKey", userId, deviceId);
+      return rpcPostMessage("keytar", {
+        method: "createPickleKey",
+        content: { userId, deviceId },
+      });
+    };
+
+    this.platform.destroyPickleKey = async (
+      userId: string,
+      deviceId: string
+    ): Promise<string | null> => {
+      console.log("destroyPickleKey", userId, deviceId);
+      return rpcPostMessage("keytar", {
+        method: "destroyPickleKey",
+        content: { userId, deviceId },
+      });
+    };
   }
 }
 
@@ -173,7 +206,7 @@ window.addEventListener("message", function (event) {
       break;
 
     default:
-      handleMessage(event.data);
+      rpcHandleMessage(event.data);
       break;
   }
 });
